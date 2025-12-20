81c73a3e1f87884245463223da003f734f10d71e
Minjoon Hong <mjhong0708@naver.com>
2025-12-20 12:14:30 +0000

    updated

--- cmd/hook.go
+++ cmd/hook.go
@@ -14,13 +14,8 @@ import (
 // hookCmd represents the hook command
 var hookCmd = &cobra.Command{
 	Use:   "hook",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A shell hook for aliases, etc.",
+	Long:  `Generates shell code to be sourced in order to set up aliases and environment variables for the dotfiles manager.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		root, _ := cmd.Flags().GetString("root")
 		dataDir := internal.DefaultDataDir()
@@ -47,14 +42,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(hookCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// hookCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// hookCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/root.go
+++ cmd/root.go
@@ -19,13 +19,8 @@ var (
 
 	rootCmd = &cobra.Command{
 		Use:   "dotfiles",
-		Short: "A brief description of your application",
-		Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+		Short: "My dotfiles manager",
+		Long:  `Manages dotfiles and configs`,
 		// Uncomment the following line if your bare application
 		// has an action associated with it:
 		// Run: func(cmd *cobra.Command, args []string) { },
